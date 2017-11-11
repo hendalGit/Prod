c860d4549249745ee8ad7d226495f92ed50240a7
Joan Cañellas Fontanilles <joan159c@gmail.com>
2017-11-11 17:51:28 +0100

    Passport callback

--- server/services/passport.js
+++ server/services/passport.js
@@ -16,9 +16,10 @@ passport.use(
       User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
           // we already have a record with the given profile ID
+          done(null, existingUser);
         } else {
           // we don't have a user record with the ID, make a new record
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id }).save().then(newUser => done(null, newUser));
         }
       });
     }

5ce1f0d7d33ebbd330628a8da86a7b60ba945eb1
Joan Cañellas Fontanilles <joan159c@gmail.com>
2017-11-12 18:51:52 +0100

    Header design

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a href="/" class="left brand-logo">
+            Emaily
+          </a>
+          <ul class="right hide-on-med-and-down">
+            <li>
+              <a href="/auth/google">Loggin with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 

23afe246f60551dd4d544984404b7f057740d429
Joan Cañellas Fontanilles <joan159c@gmail.com>
2017-11-12 20:48:31 +0100

    Access state auth

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case '':
+        <ul className="right">
+          <li>
+            <a href="/auth/google">Loggin with Google</a>
+          </li>
+        </ul>;
+
+      case false:
+        <ul className="right">
+          <li>
+            <a href="/auth/google">Loggin with Google</a>
+          </li>
+        </ul>;
+      default:
+        <ul className="right">
+          <li>
+            <a href="/auth/google">Loggin with Google</a>
+          </li>
+        </ul>;
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -8,15 +33,15 @@ class Header extends Component {
           <a href="/" className="left brand-logo">
             Emaily
           </a>
-          <ul className="right hide-on-med-and-down">
-            <li>
-              <a href="/auth/google">Loggin with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);

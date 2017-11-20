8dac54c267d12012c5bfc7a9378dc6e37688e0e6
Joan Cañellas Fontanilles <joan159c@gmail.com>
2017-11-20 13:00:35 +0100

    Fix header and custome button

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -4,7 +4,15 @@ import StripeCheckout from 'react-stripe-checkout';
 class Payments extends Component {
   render() {
     return (
-      <StripeCheckout amount={500} token={token => console.log(token)} stripeKey={process.env.REACT_APP_STRIPE_KEY} />
+      <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">ADD CREDITS</button>
+      </StripeCheckout>
     );
   }
 }

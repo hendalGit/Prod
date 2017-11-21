989c23d0f6f06834474f355414230f78bff8b81a
Joan Cañellas Fontanilles <joan159c@gmail.com>
2017-11-21 21:10:04 +0100

    Create subdoc

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -12,7 +12,9 @@ module.exports = app => {
       title,
       subject,
       body,
-      recipients
+      recipients: recipients.split(',').map(email => ({ email })),
+      _user: req.user.id,
+      dateSent: new Date()
     });
   });
 };

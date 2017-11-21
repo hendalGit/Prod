7b98fa87b56d8212af1f2c99e5e16299ab3e907b
Joan Cañellas Fontanilles <joan159c@gmail.com>
2017-11-21 20:59:39 +0100

    Create survey

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const requireLogin = require('../middlewares/requireCredits');
+const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/survey', requireLogin, requireCredits, (req, res) => {});
+  app.post('/api/survey', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients
+    });
+  });
 };
